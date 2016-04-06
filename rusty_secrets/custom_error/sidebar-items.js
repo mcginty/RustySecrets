dcf857063d9d50112f794d4fdb45747d38bbecac
Frederic Jacobs <github@fredericjacobs.com>
2016-04-07 01:14:44 +0200

    Documentation

---
+++ rusty_secrets/custom_error/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["other_io_err","Returns an io:Error from description string and optional detail string. Particularly useful in Result expressions."],["pie2io","maps a ParseIntError to an io::Error"]],"mod":[["convert","Traits for conversions between types."]],"struct":[["Error","Error struct used for generating an io:Error from a generic description."]]});
